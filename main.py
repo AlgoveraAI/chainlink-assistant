a8d9c8c0057f61d1d752cdd7b220b3a40291cc6c
josepablofm78 <jp@keyko.io>
2023-09-20 13:17:32 +0200

    cors policy

--- main.py
+++ main.py
@@ -77,14 +77,13 @@ if chain is None:
 
 app = FastAPI()
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["x-api-key"]
 )
 
 
